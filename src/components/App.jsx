4946bfa177cfb1830043722302c4452d79487e71
Andrii Shpak <SHPAKK2@GMAIL.COM>
2022-12-21 13:07:38 +0200

    firstTry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,17 +7,30 @@ import { ContactForm } from './ContactForm/ContactForm';
 import { Filter } from './Filter/Filter';
 import { ContactList } from './ContactList/ContactList';
 
-export class App extends React.Component {
-
-  state = {
-    contacts: [
+const contactsArray = [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    ]
+
+export class App extends React.Component {
+
+  state = {
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const parsedTodo = JSON.parse(localStorage.getItem("contacts")) ?? contactsArray;
+    this.setState({contacts: parsedTodo})
+   }
+
+  componentDidUpdate(preProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts))
+    }
+  }
 
   onAddBtnClick = ({name, number}) => {
     const includesName = this.state.contacts.find(
