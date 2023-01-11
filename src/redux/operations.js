b6c40833df1d00ed9ce29b59c7625d57b1844873
Andrii Shpak <SHPAKK2@GMAIL.COM>
2023-01-11 16:36:07 +0200

    install

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -3,6 +3,8 @@ import axios from "axios"
 
 const BASE_URL = "https://63bd511bd660062388a16152.mockapi.io/contacts"
 
+// BASE_URL = "https://connections-api.herokuapp.com/"
+
 export const fetchContacts = createAsyncThunk(
     "/contacts/fetchAll", async (_, thunkAPI) => {
         try {
@@ -24,6 +26,17 @@ export const addContact = createAsyncThunk(
         }
     }
 )
+
+// export const addContact = createAsyncThunk(
+//     "contacts/addContact", async ({ name, phone }, thunkAPI) => {
+//         try {
+//             const response = await axios.post(`${BASE_URL}/contacts`, { name, phone }) 
+//             return response.data
+//         } catch (e) {
+//             return thunkAPI.rejectWithValue(e.message)
+//         }
+//     }
+// )
 
 export const deleteContact = createAsyncThunk(
     "contacts/deleteContact", async (contactId, thunkAPI) => {
